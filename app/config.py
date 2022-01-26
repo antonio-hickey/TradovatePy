808c4c4915aebb2f293cd98c8641a59668a1fe21
Antonio Hickey <antoniohickey99@gmail.com>
2022-01-26 13:00:55 -0500

    build: config settings

---
+++ app/config.py
@@ -0,0 +1,7 @@
+URLs = {
+    "DEMO": "https://demo.tradovateapi.com/v1",
+    "LIVE": "https://live.tradovateapi.com/v1",
+    "MD": "wss://md.tradovateapi.com/v1/websocket",
+    "WS_DEMO": "wss://demo.tradovateapi.com/v1/websocket",
+    "WS_LIVE": "wss://live.tradovateapi.com/v1/websocket",
+}
